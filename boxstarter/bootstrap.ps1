3d7792aa02e904a37c8b44d6ebbfd3332709f930
jeremyhaugen <4142314+jeremyhaugen@users.noreply.github.com>
2019-08-03 23:21:39 -0700

    Import update-sessionenvironment command

--- boxstarter/bootstrap.ps1
+++ boxstarter/bootstrap.ps1
@@ -24,6 +24,11 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install boxstarter -y
+
+# Bootstrap the ChocolateyInstall so we can import Update-SessionEnviroment
+$env:ChocolateyInstall = Convert-Path "$((Get-Command choco).path)\..\.."
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+
 Update-SessionEnvironment
 echo $env:PSModulePath
 Import-Module Boxstarter.Chocolatey
