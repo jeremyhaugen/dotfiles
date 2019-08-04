fbf403406b3a94376f57d65c897380fa294e108c
jeremyhaugen <4142314+jeremyhaugen@users.noreply.github.com>
2019-08-03 22:49:31 -0700

    Show errors with import

--- boxstarter/bootstrap.ps1
+++ boxstarter/bootstrap.ps1
@@ -24,7 +24,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install boxstarter -y
-Import-Module Boxstarter.Chocolatey\Boxstarter.Chocolatey.psd1 -DisableNameChecking -ErrorAction SilentlyContinue
+Import-Module Boxstarter.Chocolatey\Boxstarter.Chocolatey.psd1 -DisableNameChecking
 if ($nuke)
 {
     Install-BoxstarterPackage -PackageName $NukeScript
